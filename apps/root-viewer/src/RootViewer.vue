cfd71a42e7e2fb0ec9d72f88d5f08617015e4f8d
Mohit Tyagi <mohitt@iitk.ac.in>
2019-08-12 16:53:14 +0200

    Root Viewer app

--- apps/root-viewer/src/RootViewer.vue
+++ apps/root-viewer/src/RootViewer.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="root-viewer">
     <root-viewer-app-bar />
-    <div class="uk-flex">
-      <iframe :src="iframeElement" height="800" width="800" />
+    <div class="viewer">
+      <div class="iframe-container">
+        <iframe class="viewer-iframe" :src="iframeElement" scrolling="yes" />
+      </div>
     </div>
   </div>
 </template>
@@ -31,9 +33,36 @@ export default {
   },
   methods: {
     getIframe () {
-      console.log(this.getToken)
-      this.iframeLocation = 'http://localhost:9998/iframe/open/' + this.activeFile.path.substr(1) + '?access_token=' + this.getToken
+      const url = 'http://localhost:9998/iframe/open/' + this.activeFile.path.substr(1) + '?access_token=' + this.getToken
+      fetch(url, { mode: 'cors', method: 'GET' })
+        .then(response => {
+          if (response.status === 200) {
+            return response.text().then(text => {
+              console.log(text)
+              this.iframeLocation = text
+            })
+          } else {
+            console.log(response)
+            throw new Error('get value did not worked')
+          }
+        })
+        .catch(error => {
+          console.error(error)
+        })
     }
   }
 }
 </script>
+<style>
+.viewer-iframe {
+  display: block;
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  margin: 0;
+  padding: 0;
+  padding-bottom: 25px;
+  border: 0 none;
+  box-sizing: border-box;
+}
+</style>
